28cec6900a74d57905eb291c8fc23c9f3d2b8fe8
Facundo <silvabesilfacundo@gmail.com>
2022-02-02 16:56:40 -0300

    primera version del navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './NavBarStyles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown, faCoffee, faCog } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faAngleDown} />
+
+function NavBar(){
+    return(
+        <div className='navbar-container'>
+            <nav>
+                <p className='navbar-logo'>Mercante.</p>
+                    <div className='navbar-button-container'>
+                        <p className='navbar-button'>Inicio</p>
+                        <p className='navbar-button'>Productos<FontAwesomeIcon icon={faAngleDown} size='xs'/></p>
+                        <p className='navbar-button'>FAQ</p>
+                        <p className='navbar-button'>Contacto</p>
+                    </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;  
